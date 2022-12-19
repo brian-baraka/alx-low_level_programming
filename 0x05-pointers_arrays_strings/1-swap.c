318a15a0142c529d3a00ccab099de8794b85d229
brian-baraka <nzuvebrian254@gmail.com>
2022-12-19 12:56:16 -0800

    string 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swap the var of 2 int
+ * @a: first int
+ * @b: second int
+ * return 0
+ */
+void swap_int(int *a, int *b);
+{
+ int s;
+ s = *a;
+ *a = *b;
+ *b = *s;
+}
