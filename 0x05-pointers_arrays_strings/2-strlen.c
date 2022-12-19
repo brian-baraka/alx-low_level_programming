f6c96ec4a86f04e38c0c795bd32adff52fe45cd9
brian-baraka <nzuvebrian254@gmail.com>
2022-12-19 13:52:23 -0800

    string 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * -sterlen - returns the length of a string.
+ *  @s: char to check
+ *  Return 0;
+ */
+
+int _strlen(char *s)
+{
+	int = 0; /* start the counter from 0*/
+
+	for (; *s++;)
+		a++;
+return (a);
+} 
